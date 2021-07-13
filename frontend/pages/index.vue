a4ae8830be77038aa415b7c3ce7c0b61f724aba1
zedparman <58415963+zedparman@users.noreply.github.com>
2021-07-13 13:28:16 -0700

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <h1>My New App</h1>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-for='post in posts' :key='post.id'>
+  <h3>Post Title: </h3>  {{post.title}}
+      <h3>Post Body: </h3>  {{post.body}}
+    </div>
+    <h2>Essential Links</h2>
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  name: 'myStore',
+  data () {
+    return {
+      msg: 'Welcome to my Vuex Store'
+    }
+  },
+  computed: {
+    posts() {
+    return this.$store.state.posts
+    }
+  },
+  mounted() {
+    this.$store.dispatch("getPosts");
   }
+}
 </script>
 
 <style scoped>
