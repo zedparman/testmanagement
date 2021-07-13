6164331a3ba4fe6ab67d59be5e2a2f09882ac48e
zedparman <58415963+zedparman@users.noreply.github.com>
2021-07-13 13:28:18 -0700

    Update index.js

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,25 +1,47 @@
+import axios from 'axios'
+
 export const state = () => ({
     posts: []
 })
 
-export const getters = {
-    getterValue: state => {
-        return state.value
-    }
-}
+
+export const getters = {}
 
 export const mutations = {
-    updateValue: (state, payload) => {
-        state.value = payload
+    SET_POSTS(state, posts) {
+        state.posts = posts
     }
 }
 
 export const actions = {
-    updateActionValue({ commit }) {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then((response) =>{
-        console.log(response.data)
-            commit('updateValue', response.data)
-    })
-        
+    getPosts({ commit }) {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                commit('SET_POSTS', response.data)
+        })
     }
 }
+
+
+
+// export const state = () => ({
+//     posts: []
+// })
+
+// export const getters = {
+//     getterValue: state => {
+//         return state.value
+//     }
+// }
+
+// export const mutations = {
+//     updateValue: (state, payload) => {
+//         state.value = payload
+//     }
+// }
+
+// export const actions = {
+//     updateActionValue({ commit }) {
+//         commit('updateValue', payload)
+//     }
+// }
