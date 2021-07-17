9dcfcdba98d90cad239e996a3161d654cfc44894
zedparman <58415963+zedparman@users.noreply.github.com>
2021-07-17 00:47:51 -0700

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,31 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+<h1>hello</h1>
+<div v-for="(item, index) in posts" :key="index">
+  {{ item.title }}
+</div>
+
+
+
+    <!-- <h1>{{ msg }}</h1>
     <div v-for='post in posts' :key='post.id'>
   <h3>Post Title: </h3>  {{post.title}}
       <h3>Post Body: </h3>  {{post.body}}
     </div>
-    <h2>Essential Links</h2>
+    <h2>Essential Links</h2> -->
   </div>
 </template>
 
 <script>
+
+import {mapState} from 'vuex'
 export default {
-  name: 'myStore',
-  data () {
-    return {
-      msg: 'Welcome to my Vuex Store'
-    }
-  },
-  computed: {
-    posts() {
-    return this.$store.state.posts
-    }
-  },
-  mounted() {
-    this.$store.dispatch("getPosts");
-  }
+name: 'app',
+mounted() {
+this.$store.dispatch('loadPosts')
+},
+computed: {
+...mapState([
+'posts'
+])
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default {
+//   name: 'myStore',
+//   data () {
+//     return {
+//       msg: 'Welcome to my Vuex Store'
+//     }
+//   },
+//   computed: {
+//     posts() {
+//     return this.$store.state.posts
+//     }
+//   },
+//   mounted() {
+//     this.$store.dispatch("getPosts");
+//   }
+// }
 </script>
 
 <style scoped>
