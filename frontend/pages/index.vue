1f2851311711163f64d05d238f13e86505bfcf65
zedparman <58415963+zedparman@users.noreply.github.com>
2021-07-16 23:01:02 -0700

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>{{ msg }}</h1>
-    <h1>{{ msg }}</h1>
-    <h1>{{ msg }}</h1>
-    <h1>{{ msg }}</h1>
     <div v-for='post in posts' :key='post.id'>
   <h3>Post Title: </h3>  {{post.title}}
       <h3>Post Body: </h3>  {{post.body}}
