470bf8610c9192ecfbfa7f208d685541a0cf5d67
zedparman <58415963+zedparman@users.noreply.github.com>
2021-07-22 18:36:28 -0700

    Update index.js

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -40,7 +40,27 @@ export const actions = {
 
 
 
+export const state = () => ({
+    value: 'myvalue'
+})
 
+export const getters = {
+    getterValue: state => {
+        return state.value
+    }
+}
+
+export const mutations = {
+    updateValue: (state, payload) => {
+        state.value = payload
+    }
+}
+
+export const actions = {
+    updateActionValue({ commit }) {
+        commit('updateValue', payload)
+    }
+}
 
 
 
